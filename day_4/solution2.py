678af644278f9ae31d87767f96f7ca800bd83689
Igor <ig.vp@hotmail.com>
2022-12-04 02:33:48 -0300

    dia 4, puzzle 2 done

---
+++ day_4/solution2.py
@@ -0,0 +1,19 @@
+def solution():
+    f = open("./input.txt", "r")
+    f = f.readlines()
+    f = [x.replace("\n","").strip() for x in f]
+    f = [x.split(",") for x in f]
+    pairs = []
+    for i in f:
+        pair = [x.split("-") for x in i]
+        pairs.append(pair)
+    over = 0
+    for i in pairs:
+        first = i[0]
+        second = i[1]
+        firstString = set((range(int(first[0]),int(first[1])+1)))
+        secondString = set((range(int(second[0]),int(second[1])+1)))
+        if(len(firstString.intersection(secondString)) != 0 or len(secondString.intersection(firstString)) !=0):
+            over+=1
+    print(over)
+solution()
